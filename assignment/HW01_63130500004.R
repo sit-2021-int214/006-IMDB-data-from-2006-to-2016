46cf336cae4bdff4f8dfbaed2c5a7b00371a42f6
Kittiphum601 <68773089+Kittiphum601@users.noreply.github.com>
2021-08-29 12:29:10 +0700

    HW01

--- assignment/HW01_63130500004.R
+++ assignment/HW01_63130500004.R
@@ -13,11 +13,11 @@ var(Number) #53.853
 #Exercise 2
 #2.1. Create data structure in variable named marvel_movies and explain why you using this data structure ?
 marvel_movies <- c("Iron Man","The Incredible Hulk","Iron Man 2","Thor","Captain America: The First Avenger",
-                  "The Avengers","Iron Man 3","Thor: The Dark World","Captain America: The Winter Soldier",
-                  "Guardians of the Galaxy","Avengers: Age of Ultron","Ant-Man","Captain America: Civil War",
-                  "Doctor Strange","Guardians of the Galaxy 2","Spider-Man: Homecoming","Thor: Ragnarok","Black Panther",
-                  "Avengers: Infinity War","Ant-Man and the Wasp","Captain Marvel","Avengers: Endgame",
-                  "Spider-Man: Far From Home","WandaVision","Falcon and the Winter Soldier","Loki","Black Widow")
+                   "The Avengers","Iron Man 3","Thor: The Dark World","Captain America: The Winter Soldier",
+                   "Guardians of the Galaxy","Avengers: Age of Ultron","Ant-Man","Captain America: Civil War",
+                   "Doctor Strange","Guardians of the Galaxy 2","Spider-Man: Homecoming","Thor: Ragnarok","Black Panther",
+                   "Avengers: Infinity War","Ant-Man and the Wasp","Captain Marvel","Avengers: Endgame",
+                   "Spider-Man: Far From Home","WandaVision","Falcon and the Winter Soldier","Loki","Black Widow")
 # List of released year of Marvel movies
 years <- c(2008,2008,2010,2011,2011,2012,2013,2013,2014,2014,2015,2015,2016,2016,
            2017,2017,2017,2017,2018,2018,2019,2019,2019,2021,2021,2021,2021)
